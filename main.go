5a1d848b9e394892324da43eb8c44d9ee5788127
sehoon123 <sehoon0519@naver.com>
2020-02-09 02:10:13 +0900

    1.9

--- main.go
+++ main.go
@@ -2,15 +2,8 @@ package main
 
 import "fmt"
 
-func superAdd(numbers ...int) int {
-	total := 0
-	for _, number := range numbers {
-		total += number
-	}
-	return total
-}
-
 func main() {
-	result := superAdd(1, 2, 3, 4, 5, 6)
-	fmt.Println(result)
+	names := []string{"nico", "lynn", "dal"}
+	names = append(names, "alalalal")
+	fmt.Println(names)
 }
